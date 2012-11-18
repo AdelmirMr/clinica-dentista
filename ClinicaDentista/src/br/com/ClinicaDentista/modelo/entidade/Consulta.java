4e180c023db09665f290909ecbc52ef56f11026b
amilcar.candido <amilcar.candido@gmail.com>
2012-11-18 22:00:11 +0000

    Commit Inicial

---
+++ ClinicaDentista/src/br/com/ClinicaDentista/modelo/entidade/Consulta.java
@@ -0,0 +1,21 @@
+package br.com.ClinicaDentista.modelo.entidade;
+
+import java.util.Date;
+
+public class Consulta {
+	
+	private int id;
+	
+	private Dentista dentista;
+	
+	private Paciente paciente;
+	
+	private Date dataCriacao;
+	
+	private double valor;
+	
+	private Procedimento tratamento;
+	
+	private Odontograma odontograma;		
+
+}
---
+++ ClinicaDentista/src/br/com/ClinicaDentista/modelo/entidade/Procedimento.java
@@ -0,0 +1,7 @@
+package br.com.ClinicaDentista.modelo.entidade;
+
+public class Procedimento {
+
+	
+	
+}
